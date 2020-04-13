e4b6673d882d53a590ce386a34c21fa8b5159d17
Shota <shotikotchelidze@gmail.com>
2020-04-13 17:43:36 +0400

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '654321');
+  define('DB_NAME', 'second_shareposts');
+
+  // App Root
+  define('APPROOT', (dirname(__FILE__, 2)));
+  // URL Root
+  define('URLROOT', 'http://localhost:8012/qvevri');
+  // Site Name
+  define('SITENAME', 'SharePosts');
+  // App Version
+  define('APPVERSION', '1.0.0');
